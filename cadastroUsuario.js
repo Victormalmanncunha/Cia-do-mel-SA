07ddc8a761ec16f48c3834469019efb1e14d0411
Erickandpere <165971121+Erickandpere@users.noreply.github.com>
2024-06-25 21:49:29 -0300

    Update and rename script.js to cadastroUsuario.js

--- cadastroUsuario.js
+++ cadastroUsuario.js
@@ -19,7 +19,7 @@ if (bancoDeDados == null) {
         if (usuario.login == login && usuario.senha == senha) {
             mensagem = "Parabéns, você logou!";
             localStorage.setItem("logado", JSON.stringify(usuario));
-            window.location.href = "home.html"
+            
             break;
         }
     }
