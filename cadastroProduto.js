d9573022b6d509d8f79397e35552fc0e3b808c98
Victormalmanncunha <145499088+Victormalmanncunha@users.noreply.github.com>
2024-07-17 22:02:35 -0300

    Update cadastroProduto.js

--- cadastroProduto.js
+++ cadastroProduto.js
@@ -1,3 +1,30 @@
 function paginaInicio(){
     window.location.href = "index.html"
 }
+function irPaginaCadastro(){
+    let logado = JSON.parse(localStorage.getItem("logado"));
+    if(logado == null){
+        window.location.href = "cadastroUsuario.html"
+        return;
+    }
+    localStorage.removeItem("logado")
+    document.getElementById("nomeLogado").innerHTML = `Entrar/Cadastrar-se`
+    document.getElementById("mensagemUsuario").innerHTML = `CIA DO MEL`
+    alert("VOLTE SEMPRE!!")
+    window.location.href = "index.html"
+}
+function checarLogado(){
+    let logado = JSON.parse(localStorage.getItem("logado"));
+    console.log(logado)
+    if(logado != null){
+        document.getElementById("nomeLogado").innerHTML = `${logado.login} / Deslogar`
+        document.getElementById("mensagemUsuario").innerHTML = `OLÁ ${logado.login.toUpperCase()}!`
+    }
+
+
+}
+checarLogado()
+function irPaginaCatalogo(){
+    window.location.href = "catalogo.html"
+
+}
