ffd6636977fc063e39b15dba7597bca25318cfa0
Victormalmanncunha <145499088+Victormalmanncunha@users.noreply.github.com>
2024-07-24 22:03:42 -0300

    Update cadastroProduto.js

--- cadastroProduto.js
+++ cadastroProduto.js
@@ -1,3 +1,10 @@
+const inputNome = document.getElementById("nome");
+const inputDescricao = document.getElementById("descricao");
+const inputFoto = document.getElementById("foto");
+const inputValor = document.getElementById("valor");
+let  encontrado = -1;
+
+
 function paginaInicio(){
     window.location.href = "index.html"
 }
@@ -30,8 +37,83 @@ function irPaginaCatalogo(){
 }
 
 function cadastrarProduto(){
-    let nome = document.getElementById("nome").value;
-    // let descricao = 
-    // let foto = 
-    // let valor = 
+    let nome = inputNome.value;
+    let descricao = inputDescricao.value;
+    let foto = inputFoto.value;
+    let valor = inputValor.value;
+    let produtos = JSON.parse(localStorage.getItem("produtos")) || [];
+
+    if(nome == "" || descricao == "" || foto == "" ||valor == "" ){
+        alert ("Preencha todos os campos")
+    }else {
+        for( let produto of produtos){
+            if (produto.nome == nome){
+                alert("Produto ja cadastrado")
+                return;
+            }
+
+        } 
+        let produto =  {
+            nome:nome, 
+            descricao:descricao,
+            foto:foto,
+            valor:Number(valor)
+
+        } 
+        produtos.push(produto)
+         localStorage.setItem("produtos", JSON.stringify(produtos)) 
+         alert("Produto Cadastrado com Sucesso")
+         limparFormulario()
+    } 
+    
 }
+
+function editarProduto(){
+    let produtoSelecionado = inputNome.value 
+    let produtos = JSON.parse(localStorage.getItem("produtos")) || [];
+for (i=0;i<produtos.length;i++){
+    if (produtos[i].nome==produtoSelecionado){
+        inputDescricao.value = produtos[i].descricao
+        inputFoto.value = produtos[i].foto
+        inputValor.value = produtos[i].valor
+        encontrado = i
+        alert(`${produtoSelecionado} selecionado`)
+
+    }
+
+}
+
+}
+
+function salvarEdicao (){
+    let produtos = JSON.parse(localStorage.getItem("produtos")) || [];
+    produtos[encontrado].nome=inputNome.value
+    produtos[encontrado].descricao=inputDescricao.value
+    produtos[encontrado].foto=inputFoto.value
+    produtos[encontrado].valor=inputValor.value
+    localStorage.setItem("produtos",JSON.stringify(produtos))
+    limparFormulario()
+    alert("Alterado com Sucesso")
+}
+
+function limparFormulario(){
+    inputNome.value = ""
+    inputDescricao.value =""
+    inputFoto.value = ""
+    inputValor.value = ""
+}
+
+function excluirProduto(){
+    let produtos = JSON.parse(localStorage.getItem("produtos")) || [];
+    for (let i = 0; i<produtos.length; i++){
+        if(produtos[i].nome==inputNome.value){
+            produtos.splice(i,1)
+            alert(`${inputNome.value} Excluido com Sucesso`)
+            localStorage.setItem("produtos", JSON.stringify(produtos))
+            limparFormulario()
+        }
+    }
+}
+
+
+
