53a40c27cac084e7d90eb38be1062c047988fae9
Victormalmanncunha <145499088+Victormalmanncunha@users.noreply.github.com>
2024-07-29 19:03:28 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+function irPaginaCadastro(){
+    let logado = JSON.parse(localStorage.getItem("logado"));
+    if(logado == null){
+        window.location.href = "cadastroUsuario.html"
+        return;
+    }
+    localStorage.removeItem("logado")
+    document.getElementById("nomeLogado").innerHTML = `Entrar/Cadastrar-se`
+    document.getElementById("mensagemUsuario").innerHTML = `CIA DO MEL`
+    alert("VOLTE SEMPRE!!")
+}
+function checarLogado(){
+    let logado = JSON.parse(localStorage.getItem("logado"));
+    console.log(logado)
+    if(logado != null){
+        document.getElementById("nomeLogado").innerHTML = `${logado.login} / Deslogar`
+        document.getElementById("mensagemUsuario").innerHTML = `OLÁ ${logado.login.toUpperCase()}!`
+    }
+
+
+}
+checarLogado()
+function irPaginaCatalogo(){
+    window.location.href = "catalogo.html"
+
+}
