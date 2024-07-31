64eede25b6f367c57182d4f30eefc945a9e5a854
Victormalmanncunha <145499088+Victormalmanncunha@users.noreply.github.com>
2024-07-30 21:04:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function irPaginaCadastro(){
         window.location.href = "cadastroUsuario.html"
         return;
     }
+    localStorage.removeItem("carrinho")
     localStorage.removeItem("logado")
     document.getElementById("nomeLogado").innerHTML = `Entrar/Cadastrar-se`
     document.getElementById("mensagemUsuario").innerHTML = `CIA DO MEL`
@@ -33,4 +34,46 @@ function mostrarModal(texto){
   function fecharModal(){
       document.getElementById("modal").style.display="none";
   }
+  function abrirCarrinho(){
+    document.getElementById("modalCarrinho").style.display="flex"
+}
+document.addEventListener("click", (evento)=>{
+    if(evento.target==document.getElementById("modalCarrinho")){
+        fecharCarrinho()
+    }
+})
+
+function fecharCarrinho(){
+    document.getElementById("modalCarrinho").style.display="none"
+}
+  function atualizarCarrinho(){
+    let produtosCarrinho = JSON.parse(localStorage.getItem("carrinho"));
+    if(produtosCarrinho){
+        let totalPreco = 0;
+        for(let produto of produtosCarrinho){
+            totalPreco += produto.valor * produto.quantidade;
+        }
+        document.getElementById("total").innerText=`R$${totalPreco.toFixed(2)}`
+        document.getElementById("totalCarrinho").innerText=`R$${totalPreco.toFixed(2)}`
+        document.getElementById("meioCarrinho").innerHTML=" "
+        for(i=0; i<produtosCarrinho.length; i++){
+            let preco = Number(produtosCarrinho[i].valor).toFixed(2)
+            document.getElementById("meioCarrinho").innerHTML+=`<div class="produtoCarrinho">
+            <img src="${produtosCarrinho[i].foto}" alt="">
+            <p>${produtosCarrinho[i].nome}</p>
+            <p>R$${preco}</p>
+            <p>${produtosCarrinho[i].quantidade}X</p>
+            <svg onclick="removerProdutoCarrinho(${i})" xmlns="http://www.w3.org/2000/svg" width="1vw" height="1vw" viewBox="0 0 24 24"><path fill="white" d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6z"/></svg>
+        </div>`
+        }
+    }
+}
+atualizarCarrinho()
+
+function removerProdutoCarrinho(indice){
+    let produtosCarrinho =JSON.parse(localStorage.getItem("carrinho"))
+    produtosCarrinho.splice(indice,1)
+    localStorage.setItem("carrinho",JSON.stringify(produtosCarrinho))
+    atualizarCarrinho() 
+}
   
